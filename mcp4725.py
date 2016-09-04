a2707185cb715b57b4a999f517405eb91e14decd
wayoda <e.fahle@wayoda.org>
2016-09-04 16:26:11 +0200

    Make code more readable, fix some typos

--- mcp4725.py
+++ mcp4725.py
@@ -2,29 +2,28 @@
 from machine import I2C
 
 #The MCP4725 has support from 2 addresses
-DEVICE_ADDRESS_1 = const(0x62)
-DEVICE_ADDRESS_2 = const(0x63)
+BUS_ADDRESS=[0x62,0x63]
 
-#The device supports a few PowerDown modes on startup and during operation 
+#The device supports a few power down modes on startup and during operation 
 POWER_DOWN_MODE = {'Off':0, '1k':1, '100k':2, '500k':3}
         
 class MCP4725:
-    def __init__(self,i2c, address=DEVICE_ADDRESS_1) :
+    def __init__(self,i2c, address=BUS_ADDRESS[0]) :
         self.i2c=i2c
         self.address=address
-        self._outbuf=bytearray(2)
+        self._writeBuffer=bytearray(2)
         
     def write(self,value):
         if value < 0:
             value=0
         value=value & 0xFFF
-        self.outbuf[0]=(value>>8) & 0xFF
-        self.outbuf[1]=value & 0xFF
-        return MCP4725.bus.writeto(self.address,self.outbuf)==2
+        self._writeBuffer[0]=(value>>8) & 0xFF
+        self._writeBuffer[1]=value & 0xFF
+        return self.i2c.writeto(self.address,self._writeBuffer)==2
 
     def read(self):
         buf=bytearray(5)
-        if MCP4725.bus.readfrom_into(self.address,buf) ==5:
+        if self.i2c.readfrom_into(self.address,buf) ==5:
             eeprom_write_busy=(buf[0] & 0x80)==0
             power_down=((buf[0] >> 1) & 0x03)
             value=((buf[1]<<8) | (buf[2])) >> 4
@@ -40,7 +39,11 @@ class MCP4725:
             #store the powerdown and output value in eeprom
             conf=conf | 0x60
         buf.append(conf)
+        #check value range
+        if value<0:
+            value=0
+        value=value & 0xFFF
         buf.append(value >> 4)
         buf.append((value & 0x0F)<<4)
-        return MCP4725.bus.writeto(self.address,buf)==3
+        return self.i2c.writeto(self.address,buf)==3
 
